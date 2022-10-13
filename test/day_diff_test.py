325d01f4aacac3407dded9aff447e3235220f7f4
coldtaco <36543008+coldtaco@users.noreply.github.com>
2022-10-13 15:47:21 +1100

    Detailed comments for each test case

--- test/day_diff_test.py
+++ test/day_diff_test.py
@@ -4,6 +4,9 @@ from src.date_diff import date_diff, Date
 class TestDateDiff(unittest.TestCase):
 
     def test_diff_date_simple(self):
+        """
+            Simple date difference check
+        """
         d1 = Date(1, 1, 1900)
         d2 = Date(2, 1, 1900)
 
@@ -11,18 +14,24 @@ class TestDateDiff(unittest.TestCase):
         self.assertEqual(d2 - d1, 1)
 
     def test_diff_date_simple_month(self):
+        """
+            Check day diff for one month
+        """
         d1 = Date(1, 1, 1900)
         d2 = Date(1, 2, 1900)
         self.assertEqual(d2 - d1, 31)
 
     def test_leap_year(self):
+        """
+            Check one leap year
+        """
         d1 = Date(1, 5, 1900)
         d2 = Date(1, 5, 1904)
         self.assertEqual(d2 - d1, 365 * 3 + 366)
 
     def test_leap_year_2(self):
         """
-            No leap year for 1900
+            No leap year for 1900 (one leap year)
         """
         d1 = Date(1, 1, 1900)
         d2 = Date(1, 1, 1905)
@@ -46,30 +55,46 @@ class TestDateDiff(unittest.TestCase):
 
     def test_m_d_larger(self):
         """
-            Two leap years
+            Take difference in dates, month day are both larger,
+            one year difference (test negative month, day checking)
         """
         d1 = Date(31, 12, 1900)
         d2 = Date(1, 1, 1901)
         self.assertEqual(d2 - d1, 1)
 
     def test_year_diff(self):
+        """
+            Take difference in year (non leap year)
+        """
         d1 = Date(1, 5, 1900)
         d2 = Date(1, 5, 1901)
         self.assertEqual(d2 - d1, 365)
 
     def test_same(self):
+        """
+            Test same date
+        """
         out = date_diff("01 01 1900, 01 01 1900")
         self.assertEqual(out, '01 01 1900, 01 01 1900, 0')
 
     def test_diff_1(self):
+        """
+            Provided test 1, normal date order
+        """
         out = date_diff("08 01 1995, 24 12 2005")
         self.assertEqual(out, '08 01 1995, 24 12 2005, 4003')
 
     def test_diff_2(self):
+        """
+            Provided test 2, reverse date order
+        """
         out = date_diff("15 04 1969, 12 09 1945")
         self.assertEqual(out, '12 09 1945, 15 04 1969, 8616')
 
     def test_compare_day(self):
+        """
+            Test comparing dates when days are different
+        """
         d1 = Date(1, 1, 1900)
         d2 = Date(2, 1, 1900)
 
@@ -77,6 +102,9 @@ class TestDateDiff(unittest.TestCase):
         self.assertFalse(d1 > d2)
 
     def test_compare_month(self):
+        """
+            Test comparing dates when months are different
+        """
         d1 = Date(2, 1, 1900)
         d2 = Date(1, 2, 1900)
 
@@ -84,6 +112,9 @@ class TestDateDiff(unittest.TestCase):
         self.assertTrue(d1 < d2)
 
     def test_compare_year(self):
+        """
+            Test comparing dates when years are different
+        """
         d1 = Date(31, 1, 1900)
         d2 = Date(1, 1, 1901)
 
@@ -91,6 +122,9 @@ class TestDateDiff(unittest.TestCase):
         self.assertTrue(d1 < d2)
 
     def test_compare_equal(self):
+        """
+            Test comparing dates are equal
+        """
         d1 = Date(1, 1, 1900)
         d2 = Date(1, 1, 1900)
 
